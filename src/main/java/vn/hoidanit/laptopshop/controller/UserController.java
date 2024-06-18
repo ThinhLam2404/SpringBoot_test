16c595652cbcc5012a3b5d23f74a8fe08c6a809b
ThinhLam <thinhlam240499@gmail.com>
2024-06-18 14:46:59 +0700

    import jstl lib

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,6 +2,7 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,9 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
-        return "thinh.html";
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("thinh", test);
+        return "hello";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: thinh
+  Date: 6/12/2024
+  Time: 11:54 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Hello from jsp</h1>
+<h1>
+    ${thinh}
+
+</h1>
+</body>
+</html>
